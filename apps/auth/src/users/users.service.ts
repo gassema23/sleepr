7f4466ffb4080062ff63e643fd7bdea214e2738b
Mathieu Gasse <gasse.mathieu@gmail.com>
2025-05-17 12:51:35 -0400

    feat: add cookie parser middleware and enhance user validation in UsersService

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import { UsersRepository } from './users.repository';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -10,10 +10,22 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) { }
 
   async create(createUserDto: CreateUserDto) {
+
+    await this.validateCreateUserDto(createUserDto);
+
     return this.usersRepository.create({
       ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 10)
     });
+  }
+
+  private async validateCreateUserDto(createUserDto: CreateUserDto) {
+    try {
+      await this.usersRepository.findOne({ email: createUserDto.email });
+    } catch (error) {
+      return;
+    }
+    throw new UnprocessableEntityException('User already exists');
   }
 
   async verifyUser(email: string, password: string) {
