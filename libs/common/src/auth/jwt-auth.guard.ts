4ca55f8adc88f861db1b54557f10106b33760100
Mathieu Gasse <gasse.mathieu@gmail.com>
2025-05-19 15:41:03 -0400

    feat: implement role-based access control with Roles decorator and update JwtAuthGuard

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -1,27 +1,46 @@
-import { CanActivate, ExecutionContext, Inject, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException, Logger } from "@nestjs/common";
 import { catchError, map, Observable, of, tap } from "rxjs";
 import { AUTH_SERVICE } from "../constants";
 import { ClientProxy } from "@nestjs/microservices";
 import { UserDto } from "../dto";
+import { Reflector } from "@nestjs/core";
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
+    private readonly logger = new Logger(JwtAuthGuard.name);
 
-    constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) { }
+    constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy, private readonly reflector: Reflector) { }
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
+        const jwt =
+            context.switchToHttp().getRequest().cookies?.Authentication ||
+            context.switchToHttp().getRequest().headers?.authentication;
         if (!jwt) {
             return false;
         }
+
+        const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
+
         return this.authClient.send<UserDto>('authenticate', {
             Authentication: jwt
         }).pipe(
             tap((res) => {
+                if (roles) {
+                    for (const role of roles) {
+                        if (!res.roles?.includes(role)) {
+                            this.logger.error('Not a valid roles');
+                            throw new UnauthorizedException('Unauthorized');
+                        }
+                    }
+                }
                 context.switchToHttp().getRequest().user = res;
             }),
             map(() => true),
-            catchError((err) => of(false))
+            catchError((err) => {
+                this.logger.error(err);
+                return of(false);
+            })
         );
     }
 }
